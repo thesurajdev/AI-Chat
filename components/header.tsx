2fd32f31c0622d0921baa73bbce33ba358c26b59
Suraj Kumar <thesurajdev@gmail.com>
2024-04-03 15:24:32 +0530

    Update header.tsx
    Vercel Logo removed

--- components/header.tsx
+++ components/header.tsx
@@ -65,15 +65,6 @@ export function Header() {
           <IconGitHub />
           <span className="hidden ml-2 md:flex">GitHub</span>
         </a>
-        <a
-          href="https://vercel.com/templates/Next.js/nextjs-ai-chatbot"
-          target="_blank"
-          className={cn(buttonVariants())}
-        >
-          <IconVercel className="mr-2" />
-          <span className="hidden sm:block">Deploy to Vercel</span>
-          <span className="sm:hidden">Deploy</span>
-        </a>
       </div>
     </header>
   )
